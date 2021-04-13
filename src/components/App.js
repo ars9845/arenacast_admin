cb1a3f49d18d3cdb6f6530cdf932d286a006676f
unknown <ars9845>
2021-04-13 14:38:52 +0900

    test

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../scss/style.scss';
+
+export default class App extends React.Component{
+    render(){
+        return(
+            <div>Hello World</div>
+        )
+    }
+}
